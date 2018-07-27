3f1ef13ab1779c658311f4f0b091ed44ac398140
jeremyrl <41297510+jeremyrl@users.noreply.github.com>
2018-07-27 16:35:05 -0500

    end of day 727

--- test/LEAF_Nexus_Tests/tests/api/controllers/SystemControllerTest.php
+++ test/LEAF_Nexus_Tests/tests/api/controllers/SystemControllerTest.php
@@ -26,9 +26,8 @@ final class SystemControllerTest extends DatabaseTest
      */
     public function testGetDatabaseVersion() : void
     {
-        $version = self::$reqClient->get('?a=system/dbversion');
-
+        $version = self::$reqClient->get(array('a'=>'system/dbversion'));
         $this->assertNotNull($version);
-        $this->assertEquals("4232", $version);
+        $this->assertEquals("4030", $version);
     }
 }
